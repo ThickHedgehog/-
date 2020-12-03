d26657c98a26794a88093a958a74cf7521c654ee
ThickHedgehog <57269349+ThickHedgehog@users.noreply.github.com>
2020-12-03 20:51:18 +0300

    update 3

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def inv(a):
     return a
 
 
+# Расчет при заданном aw
 @eel.expose
 def calc_a(z1, z2, m, b):
     z1 = float(z1)
@@ -52,6 +53,7 @@ def calc_xe(aw, z1, z2, m, b):
     return xe
 
 
+# Расчет при заданных х1 и х2
 @eel.expose
 def calc_atw2(xe, z1, z2, at):
     z1 = float(z1)
@@ -72,6 +74,200 @@ def calc_aw(xe, z1, z2, at, a):
     atw = calc_atw2(xe, z1, z2, at)
     aw = a * math.cos(at) / math.cos(atw)
     return aw
+
+
+# Расчет диаметров зубчатых колес
+@eel.expose
+def calc_d1(z1, m, b):
+    z1 = float(z1)
+    m = float(m)
+    b = float(b)
+    d1 = z1 * m / math.cos(b)
+    return d1
+
+
+@eel.expose
+def calc_d2(z2, m, b):
+    z2 = float(z2)
+    m = float(m)
+    b = float(b)
+    d2 = z2 * m / math.cos(b)
+    return d2
+
+
+@eel.expose
+def calc_dw1_1(aw, u):
+    aw = float(aw)
+    u = float(u)
+    dw1 = 2 * aw / (u + 1)
+    return dw1
+
+
+@eel.expose
+def calc_dw1_2(xe, z1, z2, at, a, u):
+    z1 = float(z1)
+    z2 = float(z2)
+    xe = float(xe)
+    at = float(at)
+    a = float(a)
+    u = float(u)
+    aw = calc_aw(xe, z1, z2, at, a)
+    dw1 = 2 * aw / (u + 1)
+    return dw1
+
+
+@eel.expose
+def calc_dw2_1(aw, u):
+    aw = float(aw)
+    u = float(u)
+    dw2 = 2 * aw * u * (u + 1)
+    return dw2
+
+
+@eel.expose
+def calc_dw2_2(xe, z1, z2, at, a, u):
+    z1 = float(z1)
+    z2 = float(z2)
+    xe = float(xe)
+    at = float(at)
+    a = float(a)
+    u = float(u)
+    aw = calc_aw(xe, z1, z2, at, a)
+    dw2 = 2 * aw * u * (u + 1)
+    return dw2
+
+
+@eel.expose
+def calc_y_1(aw, z1, z2, m, b):
+    z1 = float(z1)
+    z2 = float(z2)
+    m = float(m)
+    b = float(b)
+    aw = float(aw)
+    a = calc_a(z1, z2, m, b)
+    y = (aw - a) * m
+    return y
+
+
+@eel.expose
+def calc_y_2(xe, z1, z2, at, a, m):
+    z1 = float(z1)
+    z2 = float(z2)
+    xe = float(xe)
+    at = float(at)
+    a = float(a)
+    m = float(m)
+    aw = calc_aw(xe, z1, z2, at, a)
+    y = (aw - a) * m
+    return y
+
+
+@eel.expose
+def calc_dy_1(aw, z1, z2, m, b):
+    z1 = float(z1)
+    z2 = float(z2)
+    m = float(m)
+    b = float(b)
+    aw = float(aw)
+    xe = calc_xe(aw, z1, z2, m, b)
+    y = calc_y_1(aw, z1, z2, m, b)
+    dy = xe - y
+    return dy
+
+
+@eel.expose
+def calc_dy_2(xe, z1, z2, at, a, m):
+    z1 = float(z1)
+    z2 = float(z2)
+    xe = float(xe)
+    at = float(at)
+    a = float(a)
+    m = float(m)
+    y = calc_y_2(xe, z1, z2, at, a, m)
+    dy = xe - y
+    return dy
+
+
+@eel.expose
+def calc_da1_1(aw, z1, z2, m, b, x1):
+    z1 = float(z1)
+    z2 = float(z2)
+    m = float(m)
+    b = float(b)
+    aw = float(aw)
+    x1 = float(x1)
+    d1 = calc_d1(z1, m, b)
+    dy = calc_dy_1(aw, z1, z2, m, b)
+    da1 = d1 + 2 * (1 + x1 - dy) * m
+    return da1
+
+
+@eel.expose
+def calc_da1_2(xe, z1, z2, at, a, m, x1, b):
+    z1 = float(z1)
+    z2 = float(z2)
+    xe = float(xe)
+    at = float(at)
+    a = float(a)
+    b = float(b)
+    m = float(m)
+    x1 = float(x1)
+    d1 = calc_d1(z1, m, b)
+    dy = calc_dy_2(xe, z1, z2, at, a, m)
+    da1 = d1 + 2 * (1 + x1 - dy) * m
+    return da1
+
+
+@eel.expose
+def calc_da2_1(aw, z1, z2, m, b, x2):
+    z1 = float(z1)
+    z2 = float(z2)
+    m = float(m)
+    b = float(b)
+    aw = float(aw)
+    x2 = float(x2)
+    d2 = calc_d2(z2, m, b)
+    dy = calc_dy_1(aw, z1, z2, m, b)
+    da2 = d2 + 2 * (1 + x2 - dy) * m
+    return da2
+
+
+@eel.expose
+def calc_da2_2(xe, z1, z2, at, a, m, x2, b):
+    z1 = float(z1)
+    z2 = float(z2)
+    xe = float(xe)
+    at = float(at)
+    a = float(a)
+    m = float(m)
+    b = float(b)
+    x2 = float(x2)
+    d2 = calc_d2(z2, m, b)
+    dy = calc_dy_2(xe, z1, z2, at, a, m)
+    da2 = d2 + 2 * (1 + x2 - dy) * m
+    return da2
+
+
+@eel.expose
+def calc_df1(z1, m, b, x1):
+    m = float(m)
+    x1 = float(x1)
+    z1 = float(z1)
+    b = float(b)
+    d1 = calc_d1(z1, m, b)
+    df1 = d1 - 2 * (1.25 - x1) * m
+    return df1
+
+
+@eel.expose
+def calc_df2(z2, m, b, x2):
+    m = float(m)
+    x2 = float(x2)
+    z2 = float(z2)
+    b = float(b)
+    d2 = calc_d2(z2, m, b)
+    df2 = d2 - 2 * (1.25 - x2) * m
+    return df2
 
 
 eel.start("index.html", size=(1000, 1000))
